6d1ddf00296fc87c432ea0c71798cd8a473a016f
savage-lof <lomova20042004@ya.ru>
2020-11-21 22:56:20 +0300

    total

---
+++ main.py
@@ -0,0 +1,40 @@
+import sys
+import random
+from PyQt5 import uic  # Импортируем uic
+from PyQt5.QtWidgets import QApplication, QMainWindow
+from PyQt5.QtCore import Qt, QPoint
+from PyQt5.QtGui import QPainter, QColor
+
+
+class MyWidget(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        uic.loadUi('jojo.ui', self)  # Загружаем дизайн
+        self.pushButton.clicked.connect(self.paint)
+        # Обратите внимание: имя элемента такое же как в QTDesigner
+        self.do_paint = False
+
+    def paintEvent(self, event):
+        if self.do_paint:
+            qp = QPainter()
+            qp.begin(self)
+            self.draw_flag(qp)
+            qp.end()
+
+    def paint(self):
+        self.do_paint = True
+        self.repaint()
+
+    def draw_flag(self, qp):
+        qp.setBrush(QColor('#f7ff00'))
+        i = random.randint(0, 400)
+        j = random.randint(0, 300)
+        const = random.randint(10, 160)
+        qp.drawEllipse(i, j, const, const)
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = MyWidget()
+    ex.show()
+    sys.exit(app.exec_())
